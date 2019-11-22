66e89dd9a7e79e8c7285246a2252de6bb7556b27
Srihari Gokavarapu <srihari.gokavarapu@SLS.ads.valuelabs.net>
2019-11-22 12:05:00 +0530

    assignemnt2

---
+++ Reverseorder.py
@@ -0,0 +1,5 @@
+fruit_list1 = ['Apple', 'Berry', 'Cherry', 'Papaya']
+fruit_list1[1] = 'Banana'
+print (fruit_list1)
+fruit_list1.reverse()
+print (fruit_list1)
---
+++ fruitassign2.py
@@ -0,0 +1,4 @@
+fruit_list1 = ['Apple', 'Berry', 'Cherry', 'Papaya']
+fruit_list2 = fruit_list1
+print (fruit_list1[0:2])
+print (fruit_list2[2:])
