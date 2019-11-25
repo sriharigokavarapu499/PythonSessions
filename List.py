80e41cb26e654ea05a1ea67dde2e290776fbf052
Srihari Gokavarapu <srihari.gokavarapu@SLS.ads.valuelabs.net>
2019-11-25 12:22:48 +0530

    thirdassign

---
+++ List.py
@@ -0,0 +1,6 @@
+list_1 = [1,2,['a','b','c'],5,6,7]
+print (list_1 [2][1])
+list_1[2].insert(1,'d')
+print (list_1)
+list_1[2].remove('c')
+print (list_1)
---
+++ ditionary.py
@@ -0,0 +1,11 @@
+dict_ionary = {'a': 2, 'b': [1, 2, 3]}
+dict_ionary ['b'].append (4)
+print (dict_ionary)
+list_1 = [1,2,['a','b','c'],5,6,7]
+dict_ionary ['c']= list_1
+print (dict_ionary)
+dict_ionary ['c'][2].insert(1,8)
+print (dict_ionary)
+newlist= dict_ionary ['c'].pop(2)
+print (dict_ionary)
+print (newlist)
